fafed0ee0ff3371b55cdd73cca93f0ddddad9c8b
Vincibean <andrebessi00@gmail.com>
2017-09-04 17:36:44 +0200

    Updated to 2nd edition

--- src/main/scala/org/vincibean/scala/impatient/chapter8/exercise9/Creature.scala
+++ src/main/scala/org/vincibean/scala/impatient/chapter8/exercise9/Creature.scala
@@ -19,7 +19,7 @@ package org.vincibean.scala.impatient.chapter8.exercise9
 
 /**
   * In the Creature class of Section 8.10, "Construction Order and Early Definitions",
-  * on page 94, replace val range with a def. What happens when you also use a
+  * on page 98, replace val range with a def. What happens when you also use a
   * def in the Ant subclass? What happens when you use a val in the subclass?
   * Why?
   *
