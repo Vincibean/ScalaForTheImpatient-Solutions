b47d3b04bf0c0b1648447843c9453553d94e4a08
Vincibean <andrebessi00@gmail.com>
2017-09-04 17:15:30 +0200

    Updated to 2nd edition

--- src/main/scala/org/vincibean/scala/impatient/chapter8/exercise1/BankAccount.scala
+++ src/main/scala/org/vincibean/scala/impatient/chapter8/exercise1/BankAccount.scala
@@ -23,6 +23,7 @@ package org.vincibean.scala.impatient.chapter8.exercise1
   *
   *   class BankAccount(initialBalance: Double) {
   *     private var balance = initialBalance
+  *     def currentBalance = balance
   *     def deposit(amount: Double) = { balance += amount; balance }
   *     def withdraw(amount: Double) = { balance -= amount; balance }
   *   }
@@ -32,6 +33,8 @@ package org.vincibean.scala.impatient.chapter8.exercise1
 class BankAccount(initialBalance: Double) {
 
   private var balance = initialBalance
+
+  def currentBalance = balance
 
   def deposit(amount: Double): Double = {
     balance += amount
--- src/main/scala/org/vincibean/scala/impatient/chapter8/exercise1/CheckingAccount.scala
+++ src/main/scala/org/vincibean/scala/impatient/chapter8/exercise1/CheckingAccount.scala
@@ -23,6 +23,7 @@ package org.vincibean.scala.impatient.chapter8.exercise1
   *
   *   class BankAccount(initialBalance: Double) {
   *     private var balance = initialBalance
+  *     def currentBalance = balance
   *     def deposit(amount: Double) = { balance += amount; balance }
   *     def withdraw(amount: Double) = { balance -= amount; balance }
   *   }
