a088aed30fd2c40e928b60290c504cc5a1ce5146
Vincibean <andrebessi00@gmail.com>
2017-09-05 12:55:41 +0200

    Updated to 2nd edition

--- src/main/scala/org/vincibean/scala/impatient/chapter9/exercise4/package.scala
+++ src/main/scala/org/vincibean/scala/impatient/chapter9/exercise4/package.scala
@@ -17,6 +17,8 @@
 
 package org.vincibean.scala.impatient.chapter9
 
+import java.io.File
+
 import scala.io.Source
 
 /**
@@ -29,7 +31,7 @@ import scala.io.Source
 package object exercise4 {
 
   def getResourceAsDoubles(relativePath: String): Seq[Double] = Source
-    .fromInputStream(getClass.getClassLoader.getResourceAsStream(relativePath))
+    .fromFile(new File(getClass.getClassLoader.getResource(relativePath).toURI))
     .getLines()
     .toList
     .map(_.toDouble)
