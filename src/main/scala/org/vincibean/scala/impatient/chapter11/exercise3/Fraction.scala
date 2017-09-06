1db1b5664b0f81c43cbccb15ff2338a4c94e81b7
Vincibean <andrebessi00@gmail.com>
2017-09-06 15:25:57 +0200

    Updated to 2nd edition

--- src/main/scala/org/vincibean/scala/impatient/chapter11/exercise3/Fraction.scala
+++ src/main/scala/org/vincibean/scala/impatient/chapter11/exercise3/Fraction.scala
@@ -19,9 +19,18 @@ package org.vincibean.scala.impatient.chapter11.exercise3
 
 /**
   * Implement the Fraction class with operations + - * /. Normalize fractions, for
-  * example turning 15/-6 into -5/2. Divide by the greatest common divisor.
+  * example turning 15/-6 into -5/2. Divide by the greatest common divisor,
+  * like this:
   *
-  * Created by Vincibean on 11/09/16.
+  *   class Fraction(n: Int, d: Int) {
+  *     private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d);
+  *     private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d);
+  *     override def toString = s"$num/$den"
+  *     def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
+  *     def gcd(a: Int, b: Int): Int = if (b == 0) abs(a) else gcd(b, a % b)
+  *     ...
+  *   }
+  *
   */
 object Fraction {
 
@@ -33,7 +42,7 @@ class Fraction(n: Int, d: Int) {
   private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d)
   private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d)
 
-  override def toString: String = num + "/" + den
+  override def toString: String = s"$num/$den"
 
   def sign(a: Int): Int = if (a > 0) 1 else if (a < 0) -1 else 0
 
