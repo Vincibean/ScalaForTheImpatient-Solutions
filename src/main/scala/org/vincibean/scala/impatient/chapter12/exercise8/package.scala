795cb096d0fad809950f1033d08a848cb40e5430
Vincibean <andrebessi00@gmail.com>
2017-09-07 19:58:25 +0200

    Updated to 2nd edition

--- src/main/scala/org/vincibean/scala/impatient/chapter12/exercise8/package.scala
+++ src/main/scala/org/vincibean/scala/impatient/chapter12/exercise8/package.scala
@@ -18,7 +18,7 @@
 package org.vincibean.scala.impatient.chapter12
 
 /**
-  * In Section 12.8, "Currying", on page 149, you saw the `corresponds` method used
+  * In Section 12.8, "Currying", on page 164, you saw the `corresponds` method used
   * with two arrays of strings. Make a call to `corresponds` that checks whether the
   * elements in an array of strings have the lengths given in an array of integers.
   */
