9d1e60b80eb20044e401bb67f71f51850e3c11c1
Vincibean <andrebessi00@gmail.com>
2017-09-11 18:35:56 +0200

    Added license and documentation to exercise 15.1

--- src/main/scala/org/vincibean/scala/impatient/chapter15/exercise1/ListSpec.scala
+++ src/main/scala/org/vincibean/scala/impatient/chapter15/exercise1/ListSpec.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2017  Vincibean <Andrea Bessi>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.vincibean.scala.impatient.chapter15.exercise1
 
 import java.util.NoSuchElementException
@@ -5,6 +22,10 @@ import java.util.NoSuchElementException
 import org.junit.Test
 import org.junit.Assert._
 
+/**
+  * Write four JUnit test cases that use the @Test annotation with and without each
+  * of its arguments. Run the tests with JUnit.
+  */
 class ListSpec {
 
   val ints = List(1, 2, 3, 4)
